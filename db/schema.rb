bba9ef40865f7b82b62e0f7d5da23426a318dafb
kuima <kumagaikousuke1995@gmail.com>
2015-10-29 11:22:57 +0900

    http://openbook4.me/projects/92/sections/499

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029020844) do
+ActiveRecord::Schema.define(version: 20151029021950) do
+
+  create_table "tweets", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
