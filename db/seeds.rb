4ce2d3db848fdb85fa9054a128665b7f4728abbf
kuima <kumagaikousuke1995@gmail.com>
2015-10-29 11:12:52 +0900

    http://openbook4.me/projects/92/sections/497

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,17 @@
+@user = User.new
+@user.name = 'hogehoge'
+@user.username = 'hoge'
+@user.location = 'Kanagawa, Japan'
+@user.about = 'Hello, I am hofe. I am from database!'
+@user.save
+
+@user = User.new
+@user.name = 'hugahuga'
+@user.username = 'huga'
+@user.location = 'Nityoume, Japan'
+@user.about = 'Nice to meet you. I am from database!'
+@user.save
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
